cb55d9d30010dbb976014d7e954b97de4d9ed0e7
yazoo1220 <podman1220@hotmail.com>
2023-04-11 00:43:52 +0300

    Changed the title and made it an agent

--- main.py
+++ main.py
@@ -5,21 +5,32 @@ import os
 
 os.environ['OPENAI_API_KEY']='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'
 
-from langchain.chains import ConversationChain
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+from langchain.chat_models import ChatOpenAI
 from langchain.llms import OpenAI
 
 
-def load_chain():
+def load_agent():
     """Logic for loading the chain you want to use should go here."""
-    llm = OpenAI(temperature=0)
-    chain = ConversationChain(llm=llm)
-    return chain
+    # First, let's load the language model we're going to use to control the agent.
+    chat = ChatOpenAI(temperature=0)
 
-chain = load_chain()
+    # Next, let's load some tools to use. Note that the `llm-math` tool uses an LLM, so we need to pass that in.
+    llm = OpenAI(temperature=0)
+    tools = load_tools(["serpapi", "llm-math"], llm=llm)
+
+
+    # Finally, let's initialize an agent with the tools, the language model, and the type of agent we want to use.
+    agent = initialize_agent(tools, chat, agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+    return agent
+
+agent = load_agent
 
 # From here down is all the StreamLit UI.
-st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
-st.header("LangChain Demo")
+st.set_page_config(page_title="有能な新人君GPT", page_icon=":robot:")
+st.header("有能な新人君GPT")
 
 if "generated" not in st.session_state:
     st.session_state["generated"] = []
@@ -29,14 +40,14 @@ if "past" not in st.session_state:
 
 
 def get_text():
-    input_text = st.text_input("You: ", "Hello, how are you?", key="input")
+    input_text = st.text_input("You: ", "You are a youtube video and we can discus in details of the video. If it's fine, let's start", key="input")
     return input_text
 
 
 user_input = get_text()
 
 if user_input:
-    output = chain.run(input=user_input)
+    output = agent.run(input=user_input)
 
     st.session_state.past.append(user_input)
     st.session_state.generated.append(output)
